7d87cdc8408a21e51061c27a095b27d1b5c1ac12
ShadowS8 <jadielson.vlog7@gmail.com>
2020-01-23 18:36:49 -0300

    Ferramenta Para Baixa Video1.0

--- Modulos/Body/Menu.py
+++ Modulos/Body/Menu.py
@@ -1,19 +1,14 @@
+import random
 from time import sleep
 import os, glob
 def menu():
-        print("""
-        __   __          ____
-        \ \ / /__  _   _|  _ \  _____      __
-         \ V / _ \| | | | | | |/ _ \ \ /\ / /
-          | | (_) | |_| | |_| | (_) \ V  V /
-          |_|\___/ \__,_|____/ \___/ \_/\_/
-                <==================>
-                   [1] Videos
-                   [2] Musica
-                   [3] PlayList
-                   [4] Lista Tudo
-                   [5] Sair
-                <==================>
+        print("""		<===============================>
+	                   [1] Videos
+	                   [2] Musica
+	                   [3] PlayList
+	                   [4] Lista Tudo
+	                   [5] Sair
+                <===============================>
         """)
 def loop():
     loop = 0
@@ -51,17 +46,45 @@ def baixa():
         os.system("clear")
         print("Aguardem...")
         baixa = baixa + 1
-def lista():
-    os.chdir("/data/data/com.termux/files/home/YouDow")
+def listaM():
+    os.chdir("/data/data/com.termux/files/home/storage/shared/Music")
     a = glob.glob("*.mp3")
     b = glob.glob("*.webm")
     c = glob.glob("*.mkv")
-    print("                 LISTA DE DOWNLOADS")
+    d = glob.glob("*.mp4")
+    print("                 LISTA DE MUSICA (Music)")
     print("===========================================================")
     for ar1 in a:
         print(ar1)
     for ar2 in b:
         print(ar2)
+    print("===========================================================")
+def listaV():
+    os.chdir("/data/data/com.termux/files/home/storage/shared/Movies")
+    c = glob.glob("*.mkv")
+    d = glob.glob("*.mp4")
+    print("                 LISTA DE VIDEOS (Movies)")
+    print("===========================================================")
     for ar3 in c:
         print(ar3)
+    for ar4 in d:
+        print(ar4)
     print("===========================================================")
+def logo1():
+    os.system("figlet -c YouDow")
+def logo2():
+    os.system("figlet -c -f banner YouDow")
+def logo3():
+    os.system("figlet -c -f slant YouDow")
+def logo4():
+    os.system("figlet -c -f small YouDow")
+def logo5():
+    os.system("figlet -c -f smslant YouDow")
+
+lo1 = logo1
+lo2 = logo2
+lo3 = logo3
+lo4 = logo4
+lo5 = logo5
+logos = [lo1, lo2, lo3, lo4, lo5]
+escolha = random.choice(logos)
--- Modulos/YouDow.py
+++ Modulos/YouDow.py
@@ -5,36 +5,38 @@ os.system("clear")
 Menu.loop()
 url = 0
 while url != 5:
+    Menu.escolha()
     Menu.menu()
     try:
         url = input("OPÇÃO: ")
         if url == "1":
             url1 = input("URL: ")
             Menu.baixa()
-            os.system(f"youtube-dl --audio-format mp3 {url1}")
+            os.system(f"cd /data/data/com.termux/files/home/storage/shared/Movies && youtube-dl --audio-format mp3 {url1}")
             os.system("clear")
             continue
         elif url == "2":
             url1 = input("URL: ")
             Menu.baixa()
-            os.system(f"youtube-dl --extract-audio --audio-format mp3 {url1}")
+            os.system(f"cd /data/data/com.termux/files/home/storage/shared/Music && youtube-dl --extract-audio --audio-format mp3 {url1}")
             os.system("clear")
             continue
         elif url == "3":
             url1 = input("URL: ")
             Menu.baixa()
-            os.system(f"youtube-dl -cit {url1}")
+            os.system(f"cd /data/data/com.termux/files/home/storage/shared/Movies && youtube-dl -cit {url1}")
             os.system("clear")
             continue
         elif url == "4":
             os.system("clear")
-            Menu.lista()
+            Menu.listaM()
+            Menu.listaV()
             continue
         elif url == "5":
             url = url + 1
         else:
             os.system("clear")
-            print("Opcao Invalida... Tente Novamente")
+            print("\033[1;31mOpcao Invalida... Tente Novamente\033[m")
     except:
         print("Bye")
         os.system("exit")
